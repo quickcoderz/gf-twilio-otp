0fa437fba7a820584893f85bb7887946cc421e10
coder-manjeet <codermanjeet@gmail.com>
2025-02-22 12:52:38 +0530

    initial setup

---
+++ gf-twilio-otp.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Gravity Forms Add-on - Twilio OTP
+ * Description: Seamlessly integrates Twilio SMS OTP functionality with Gravity Forms, enabling customizable message templates and flexible timing configurations for enhanced security and user verification.
+ * Version: 1.0.0
+ * Author: Quickcoderz
+ * Author URL: https://quickcoderz.com
+ * Text Domain: gf-twilio-otp
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+
+
